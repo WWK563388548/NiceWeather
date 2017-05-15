c5ca70a7e07fb2d2773f1144f9a8c2ebe58aa806
wc563388548@gmail.com <wc563388548@gmail.com>
2017-05-15 19:41:53 +0900

    Get background from bing

--- app/src/main/java/com/example/wwk/niceweather/WeatherActivity.java
+++ app/src/main/java/com/example/wwk/niceweather/WeatherActivity.java
@@ -6,11 +6,13 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.wwk.niceweather.gson.Forecast;
 import com.example.wwk.niceweather.gson.Weather;
 import com.example.wwk.niceweather.util.HttpUtil;
@@ -48,12 +50,15 @@ public class WeatherActivity extends AppCompatActivity {
     private TextView carWashText;
 
     private TextView sportText;
+
+    private ImageView bingImg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
         // Initialize each widget
+        bingImg = (ImageView) findViewById(R.id.bing_img);
         weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
         titleCity = (TextView) findViewById(R.id.title_city);
         titleUpdateTime = (TextView) findViewById(R.id.title_update_time);
@@ -77,6 +82,13 @@ public class WeatherActivity extends AppCompatActivity {
             String weatherId = getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);
             requestWeather(weatherId);
+        }
+
+        String bingImage = preferences.getString("bing_pic", null);
+        if (bingImage != null) {
+            Glide.with(this).load(bingImage).into(bingImg);
+        } else {
+            loadBingImage();
         }
     }
 
@@ -117,6 +129,38 @@ public class WeatherActivity extends AppCompatActivity {
                         Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
                     }
                 });
+            }
+
+        });
+        loadBingImage();
+    }
+
+    /**
+     * Load Image from bing
+     */
+    private void loadBingImage() {
+        String requestBingImage = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingImage, new Callback() {
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                final String bingImage = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager
+                        .getDefaultSharedPreferences(WeatherActivity.this).edit();
+                editor.putString("bing_pic", bingImage);
+                editor.apply();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(WeatherActivity.this).load(bingImage).into(bingImg);
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
             }
 
         });
