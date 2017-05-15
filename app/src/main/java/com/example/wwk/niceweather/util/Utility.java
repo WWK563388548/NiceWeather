8811322f47977c6d51a3885de251f7f627ea74e6
wc563388548@gmail.com <wc563388548@gmail.com>
2017-05-15 09:22:25 +0900

    Update Utility

--- app/src/main/java/com/example/wwk/niceweather/util/Utility.java
+++ app/src/main/java/com/example/wwk/niceweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.wwk.niceweather.db.City;
 import com.example.wwk.niceweather.db.County;
 import com.example.wwk.niceweather.db.Province;
+import com.example.wwk.niceweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -90,5 +92,21 @@ public class Utility {
         return false;
     }
 
+    /**
+     * Handle returned JSON data of weather
+     */
+    public static Weather handleWeatherResponse(String response) {
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 
 }
