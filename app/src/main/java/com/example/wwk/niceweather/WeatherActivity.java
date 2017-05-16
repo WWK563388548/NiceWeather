7339c6b3b2e7bca0a2dcd5d8f35e15b71e20558d
wc563388548@gmail.com <wc563388548@gmail.com>
2017-05-16 12:03:00 +0900

    Integrated background and status bar

--- app/src/main/java/com/example/wwk/niceweather/WeatherActivity.java
+++ app/src/main/java/com/example/wwk/niceweather/WeatherActivity.java
@@ -1,6 +1,8 @@
 package com.example.wwk.niceweather;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -55,6 +57,14 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Integrated background and status bar
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_weather);
 
         // Initialize each widget
